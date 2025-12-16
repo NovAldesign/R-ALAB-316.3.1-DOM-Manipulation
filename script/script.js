a62f78ce9e1bcf8bfcf16ceaf84b166e0930a8f8
NovAldesign <williams.lavon@gmail.com>
2025-12-16 17:33:38 -0500

    completed first section

---
+++ script/script.js
@@ -0,0 +1,5 @@
+const mainEl = document.querySelector('main');
+mainEl.style.backgroundColor = 'var(--main-bg)';
+const h1 = document.createElement('h1');
+h1.textContent = 'DOM Manipulation'
+mainEl.classList.add('flex-ctr');
