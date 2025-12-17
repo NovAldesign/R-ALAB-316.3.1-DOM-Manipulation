400a2d3afab364405378bd3cd06ae73c16f73a4c
NovAldesign <williams.lavon@gmail.com>
2025-12-17 17:05:26 -0500

    part 2 and 3

--- script/index.js
+++ script/index.js
@@ -50,3 +50,15 @@ for (let link of menuLinks) {
   // 5.   Append the new element to the topMenuEl element.
   topMenuEl.appendChild(anchor);
 }
+//1. Select and cache the <nav id="sub-menu"> element in a variable named subMenuEl
+const subMenuEl = document.getElementById('sub-menu');
+
+//2. Set the height subMenuEl element to be "100%" 
+subMenuEl.style.height = '100%';
+
+//3. Set the background color of subMenuEl to the value stored in the --sub-menu-bg CSS custom property
+subMenuEl.style.backgroundColor = ('--sub-menu-bg');
+
+//4. Add the class of flex-around to the subMenuEl element
+subMenuEl.classList.add("flex-around");
+
